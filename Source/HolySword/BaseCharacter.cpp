19da24dad864ed4088763a0e1c6f53359070339c
Aventador <419460185@qq.com>
2024-02-02 23:56:47 +0800

    add baseCharacter class

---
+++ Source/HolySword/BaseCharacter.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseCharacter.h"
+
+ABaseCharacter::ABaseCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
---
+++ Source/HolySword/BaseCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseCharacter.generated.h"
+
+UCLASS()
+class HOLYSWORD_API ABaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABaseCharacter();
+
+};
